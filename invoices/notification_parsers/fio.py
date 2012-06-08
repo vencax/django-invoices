89bb0157d77daa3cbcf43d8bcd4e91a6f35a325a
Vaclav Klecanda <vencax77@gmail.com>
2012-06-08 12:18:29 +0200

    notification parsers update

--- invoices/notification_parsers/fio.py
+++ invoices/notification_parsers/fio.py
@@ -4,28 +4,30 @@ Created on Jan 2, 2012
 
 @author: vencax
 '''
-from test import sendTestMessage
 import re
+from test import sendTestMessage
 
 class Parser(object):
-  amountRe = re.compile(r'Částka:[ ]*(?P<Amount>[0-9,]+)')
-  VSRe = re.compile(r'VS:[ ]*(?P<VS>\d+)')
-  
-  def parse(self, message):
-    am = self.amountRe.search(message).group('Amount')
-    vs = self.VSRe.search(message).group('VS')
-    amount = float(am.replace(',', '.'))
-    vs = int(vs)
-    return (vs, amount)
+    amountRe = re.compile(u'Částka:[ ]*(?P<Amount>[0-9,]+)')
+    VSRe = re.compile(u'VS:[ ]*(?P<VS>\d+)')
+    SSRe = re.compile(u'SS:[ ]*(?P<SS>\d+)')
+    accountRe = re.compile(u'Výdaj na kontě:[ ]*(?P<acc>\d+)')
+    
+    def parse(self, message):
+        am = self.amountRe.search(message).group('Amount')
+        vs = self.VSRe.search(message).group('VS')
+        amount = float(am.replace(',', '.'))
+        vs = int(vs)
+        accNum = '%s/2010' % self.accountRe.search(message).group('acc')
+        ss = int(self.SSRe.search(message).group('SS'))
+        return (vs, ss, amount, accNum, 'CZK')
 
 if __name__ == "__main__":
-  sendTestMessage("""
-Výdaj na kontě: 2500109888
-Částka: 640,80
-VS: 1
-US:  ALBERT HM 2799 PRAH
-Aktuální zůstatek: 739,94
-Protiúčet: platba kartou
-SS: 6761657716
-KS:
-""")
+    parser = Parser()
+    data = u"""
+Výdaj na kontě: 2500109888 Částka: 640,80 VS: 1 US: \
+ALBERT HM 2799 PRAH Aktuální zůstatek: 739,94 \
+Protiúčet: platba kartou SS: 6761657716 KS:
+"""
+    print parser.parse(data)
+    sendTestMessage(data)
--- invoices/notification_parsers/test.py
+++ invoices/notification_parsers/test.py
@@ -4,14 +4,14 @@ Created on Jan 2, 2012
 @author: vencax
 '''
 def sendTestMessage(msg):
-  fromAddr = 'banka@banka.cz'
-  toAddrs = ['ahoj@koko.cz']
-  
-  message = ("From: %s\r\nTo: %s\r\n\r\n" % (fromAddr, ", ".join(toAddrs)))
-  message += msg
-  message += '\r\n'
-  
-  import smtplib
-  c = smtplib.SMTP('localhost', 2525)
-  c.sendmail(fromAddr, toAddrs, msg)
-  c.quit()
+    fromAddr = 'banka@banka.cz'
+    toAddrs = ['credit@vpn.vxk.cz']
+    
+    message = ("From: %s\r\nTo: %s\r\n\r\n" % (fromAddr, ", ".join(toAddrs)))
+    message += msg
+    message += '\r\n'
+    
+    import smtplib
+    c = smtplib.SMTP('localhost', 8025)
+    c.sendmail(fromAddr, toAddrs, msg)
+    c.quit()
