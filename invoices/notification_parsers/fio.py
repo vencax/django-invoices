8af37bd1b7493d19213cad03dc875f00e1095618
Vaclav Klecanda <vencax77@gmail.com>
2012-07-06 09:37:52 +0200

    fio parser update - parse win-1250 coding which is actually sent from fio

--- invoices/notification_parsers/fio.py
+++ invoices/notification_parsers/fio.py
@@ -7,27 +7,34 @@ Created on Jan 2, 2012
 import re
 
 class Parser(object):
-    amountRe = re.compile(u'Částka:[ ]*(?P<Amount>[0-9,]+)')
+    amountRe = re.compile(u'=C8=E1stka:[ ]*(?P<Amount>[0-9,]+)')
     VSRe = re.compile(u'VS:[ ]*(?P<VS>\d+)')
-    SSRe = re.compile(u'SS:[ ]*(?P<SS>\d+)')
-    accountRe = re.compile(u'Výdaj na kontě:[ ]*(?P<acc>\d+)')
+    SSRe = re.compile(u'SS:[ =]*(?P<SS>\d+)')
+    partnerAccountRe = re.compile('Proti=FA=E8et:[ ]*(?P<acc>[0-9]{1,10}/[0-9]{4})')
+    accountRe = re.compile(u' na kont=EC:[ ]*(?P<acc>\d+)')
+    inTransRe = re.compile(u'P=F8=EDjem na kont=EC')
     
     def parse(self, message):
+        op = self.inTransRe.search(message)
+        if op:
+            transactionType = 'IN'
+        else:
+            transactionType = 'OUT'
         am = self.amountRe.search(message).group('Amount')
         vs = self.VSRe.search(message).group('VS')
         amount = float(am.replace(',', '.'))
         vs = int(vs)
+        partnerAcc = self.partnerAccountRe.search(message).group('acc')
         accNum = '%s/2010' % self.accountRe.search(message).group('acc')
         ss = int(self.SSRe.search(message).group('SS'))
-        return (vs, ss, amount, accNum, 'CZK')
+        return (transactionType, vs, ss, amount, accNum, partnerAcc, 'CZK')
 
 if __name__ == "__main__":
     parser = Parser()
-    data = u"""
-Výdaj na kontě: 2500109888 Částka: 640,80 VS: 1 US: \
-ALBERT HM 2799 PRAH Aktuální zůstatek: 739,94 \
-Protiúčet: platba kartou SS: 6761657716 KS:
+    data = u"""P=F8=EDjem na kont=EC: 2400260986 =C8=E1stka: 500,00 VS: 1\
+ Zpr=E1va p=F8=EDjemci: =20 Aktu=E1ln=ED z=F9statek: 20 144,82\
+ Proti=FA=E8et: 2500109888/2010 SS:=20 KS: 0008
 """
     print parser.parse(data)
     from test import sendTestMessage
-    sendTestMessage(data, 'credit@domain.cz')
+    sendTestMessage(data, 'credit@domain.tld')
