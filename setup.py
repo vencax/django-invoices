64c9a241da294ddec830f8e215413416be8a3ece
Vaclav Klecanda <vencax77@gmail.com>
2013-01-15 19:47:59 +0100

    automatic compilemessages

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
+import os
+import subprocess
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 
-print find_packages()
+
+class MyInstall(install):
+    def run(self):
+        projpath = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                'invoices')
+        print 'Generating MO files in %s' % projpath
+        subprocess.call(['django-admin.py', 'compilemessages'],
+                        cwd=projpath)
+        install.run(self)
 
 setup(
     name='django-invoices',
-    version='0.1',
-    description='Simple Django invoices application.',
+    version='0.2',
+    description='Simple Django invoices application',
     author='Vaclav Klecanda',
     author_email='vencax77@gmail.com',
-    url='https://github.com/applecat/django-simple-poll',
+    url='www.vxk.cz',
     packages=find_packages(),
     include_package_data=True,
+    cmdclass={'install': MyInstall},
 )
