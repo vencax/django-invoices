69b178886a8f8084474814e03224ea7ca378f3dd
Vaclav Klecanda <vencax77@gmail.com>
2013-01-17 21:30:36 +0100

    tests

---
+++ invoices/tests.py
@@ -0,0 +1,77 @@
+import os
+from django.test import TestCase
+from django.core.management import call_command
+from invoices.models import Invoice, CompanyInfo, Item, BadIncommingTransfer
+from django.conf import settings
+from django.contrib.auth.models import User
+
+
+class InvoiceTest(TestCase):
+
+    def setUp(self):
+        TestCase.setUp(self)
+        for i in range(settings.OUR_COMPANY_ID):
+            u = User(username='user%i' % i, last_name='Mr. %i' % i,
+                     first_name='TJ', password='jfcisoa')
+            u.save()
+            CompanyInfo(town='town%i' % i, phone=(1000 + i), user=u).save()
+
+        s = CompanyInfo.objects.all().order_by('?')[0]
+        invoice = Invoice(subscriber=s)
+        invoice.save()
+        invoice.items.add(Item(name='socks', count=2, price=13))
+
+    def testAccountNotification(self):
+        """
+        Test account notification command.
+        """
+        invoice = Invoice.objects.all().order_by('?')[0]
+        mail = 'P=F8=EDjem na kont=EC: 2400260986 =C8=E1stka: %i,00 VS: %i\
+ Zpr=E1va p=F8=EDjemci: =20 Aktu=E1ln=ED z=F9statek: 20 144,82\
+ Proti=FA=E8et: 321-2500109888/2010 SS:=117 KS: 0008'
+        baseArgs = ('credit@vpn.vxk.cz', 'automat@fio.cz')
+
+        args = baseArgs + (
+            mail % (invoice.totalPrice() - 4, invoice.id),
+        )
+        call_command('accountNotification', *args)
+
+        i = Invoice.objects.get(id=invoice.id)
+        assert i.paid == False
+        try:
+            BadIncommingTransfer.objects.get(typee='l', invoice=i)
+        except BadIncommingTransfer.DoesNotExist:
+            raise AssertionError('BadIncommingTransfer not exists')
+
+        args = baseArgs + (
+            mail % (invoice.totalPrice() + 4, invoice.id),
+        )
+        call_command('accountNotification', *args)
+
+        i = Invoice.objects.get(id=invoice.id)
+        assert i.paid == False
+        try:
+            BadIncommingTransfer.objects.get(typee='m', invoice=i)
+        except BadIncommingTransfer.DoesNotExist:
+            raise AssertionError('BadIncommingTransfer not exists')
+
+        args = baseArgs + (
+            mail % (invoice.totalPrice(), invoice.id),
+        )
+        call_command('accountNotification', *args)
+
+        i = Invoice.objects.get(id=invoice.id)
+        assert i.paid == True
+
+    def test_pdf_generate(self):
+        """
+        Tests invoice PDF generation.
+        """
+        outFile = '/tmp/invoice.pdf'
+        i = Invoice.objects.all().order_by('?')[0]
+
+        from invoices.mailing import _pdfInvoice
+        with open(outFile, 'w') as f:
+            f.write(_pdfInvoice(i))
+
+        assert os.stat(outFile).st_size > 0
