007356e976128c49b0d383febd7df75970773d35
Vaclav Klecanda <vencax77@gmail.com>
2013-06-25 07:53:32 +0200

    FIO parser more robust

--- invoices/notification_parsers/fio.py
+++ invoices/notification_parsers/fio.py
@@ -1,38 +1,33 @@
 # coding=UTF-8
-'''
-Created on Jan 2, 2012
-
-@author: vencax
-'''
 import re
 
 
 class Parser(object):
-    amountRe = re.compile(u'=C8=E1stka:[ ]*(?P<Amount>[0-9 ]+,[0-9]{1,2})')
-    VSRe = re.compile(u'VS:[ ]*(?P<VS>\d+)')
-    SSRe = re.compile(u'SS:[ =]*(?P<SS>\d+)')
-    KSRe = re.compile(u'KS:[ =]*(?P<KS>\d+)')
+    """
+    Parses mail content fio FIO bank.
+    """
+
+    amountRe = re.compile(u'=C8=E1stka:[ ]*(?P<GNAME>[0-9 ]+,[0-9]{1,2})')
+    VSRe = re.compile(u'VS:[ ]*(?P<GNAME>\d+)')
+    SSRe = re.compile(u'SS:[ =]*(?P<GNAME>\d+)')
+    KSRe = re.compile(u'KS:[ =]*(?P<GNAME>\d+)')
     partnerAccountRe = re.compile('Proti=FA=E8et:[ ]*\
-(?P<acc>[0-9]*-*[0-9]{1,10}/[0-9]{4})')
-    accountRe = re.compile(u' na kont=EC:[ ]*(?P<acc>\d+)')
+(?P<GNAME>[0-9]*-*[0-9]{1,10}/[0-9]{4})')
+    accountRe = re.compile(u' na kont=EC:[ ]*(?P<GNAME>\d+)')
     inTransRe = re.compile(u'P=F8=EDjem na kont=EC')
 
     def parse(self, message):
-        op = self.inTransRe.search(message)
-        if op:
-            transactionType = 'IN'
-        else:
-            transactionType = 'OUT'
-        am = self.amountRe.search(message).group('Amount')
-        vs = self.VSRe.search(message).group('VS')
-        amount = float(am.replace(' ', '').replace(',', '.'))
-        vs = int(vs)
-        partnerAcc = self.partnerAccountRe.search(message).group('acc')
-        accNum = '%s/2010' % self.accountRe.search(message).group('acc')
-        ss = int(self.SSRe.search(message).group('SS'))
-        ks = int(self.KSRe.search(message).group('KS'))
+        direction = 'IN' if self.inTransRe.search(message) else 'OUT'
+        amount = float(self._val_or_none(self.amountRe, message).\
+                   replace(' ', '').replace(',', '.'))
+
+        vs = self._val_or_none(self.VSRe, message, lambda x: int(x))
+        partnerAcc = self._val_or_none(self.partnerAccountRe, message)
+        accNum = '%s/2010' % self._val_or_none(self.accountRe, message)
+        ss = self._val_or_none(self.SSRe, message, lambda x: int(x))
+        ks = self._val_or_none(self.KSRe, message, lambda x: int(x))
         return {
-            'direction': transactionType,
+            'direction': direction,
             'varSymb': vs,
             'specSymb': ss,
             'constSymb': ks,
@@ -42,6 +37,12 @@ class Parser(object):
             'currency': 'CZK'
         }
 
+    def _val_or_none(self, regexp, line, transfunc=lambda x: x):
+        try:
+            return transfunc(regexp.search(line).group('GNAME'))
+        except Exception:
+            return None
+
 if __name__ == "__main__":
     parser = Parser()
     data = u"""P=F8=EDjem na kont=EC: 2400260986 =C8=E1stka: 100,00 VS: 19\
@@ -49,5 +50,9 @@ if __name__ == "__main__":
  Proti=FA=E8et: 321-2500109888/2010 SS:=20 KS: 0008
 """
     print parser.parse(data)
-    from test import sendTestMessage
-    sendTestMessage(data, 'credit@domain.tld')
+
+    weirddata = u"""P=F8=EDjem na kont=EC: 2400260986 =C8=E1stka: 100,00\
+ VS: 19 Zpr=E1va p=F8=EDjemci: =20 Aktu=E1ln=ED z=F9statek: 20 144,82\
+ Proti=FA=E8et: 321-2500109888/2010 SS:=
+"""
+    print parser.parse(weirddata)
