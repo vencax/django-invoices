88f192b93b650813c89974bb908b1e9bb151bd73
Václav Klecanda <vencax@centrum.cz>
2014-11-04 22:46:07 +0100

    Update setup.py
    removed autocompilation of MOs

--- setup.py
+++ setup.py
@@ -1,17 +1,5 @@
-import os
-import subprocess
+
 from setuptools import setup, find_packages
-from setuptools.command.install import install
-
-
-class MyInstall(install):
-    def run(self):
-        projpath = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                'invoices')
-        print 'Generating MO files in %s' % projpath
-        subprocess.call(['django-admin.py', 'compilemessages'],
-                        cwd=projpath)
-        install.run(self)
 
 setup(
     name='django-invoices',
@@ -19,8 +7,7 @@ setup(
     description='Simple Django invoices application',
     author='Vaclav Klecanda',
     author_email='vencax77@gmail.com',
-    url='www.vxk.cz',
+    url='https://github.com/vencax/django-invoices',
     packages=find_packages(),
-    include_package_data=True,
-    cmdclass={'install': MyInstall},
+    include_package_data=True
 )
